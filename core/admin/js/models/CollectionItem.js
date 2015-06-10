bfcd60a9c9d8f5ad85e1953acbaa310fb5c2a195
Markus Boelter <markus.boelter@frogdesign.com>
2015-06-10 13:34:20 +1000

    Toast when saving a new item

--- core/admin/js/models/CollectionItem.js
+++ core/admin/js/models/CollectionItem.js
@@ -42,11 +42,12 @@ CollectionItem.prototype = {
       } else {
         // create
         H.postJSON('/json/collection/' + self._collection_slug, self.toJSON(), function(item) {
-          var item = JSON.parse(item),
-              toast = new Toast('Saved.');
-              
           // redirect to edit              
           window.location.href = '#/collection/' + self._collection_slug + '/edit/' + item._id;
+          
+          setTimeout(function() {
+            var toast = new Toast('Saved.');
+          }, 200);
         });
       }
     });
